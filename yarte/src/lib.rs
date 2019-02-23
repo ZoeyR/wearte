1fc305a93fbbbd24ae0f69008ab4a1ecd88a03c1
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-02-23 15:02:45 +0100

    Fix reorder imports and remove some licence infringe

--- yarte/src/lib.rs
+++ yarte/src/lib.rs
@@ -1,31 +1,26 @@
 use std::{fmt, fs};
 
-pub use yarte_helpers::{helpers, helpers::MarkupDisplay, Error, Result};
+use yarte_config::{read_config_file, Config};
+pub use yarte_derive::*;
+pub use yarte_helpers::{helpers::MarkupDisplay, Error, Result};
 
-/// Main `Template` trait; implementations are generally derived
+// TODO: document
 pub trait Template: fmt::Display {
-    /// Helper method which allocates a new `String` and renders into it
     fn render(&self) -> Result<String> {
         let mut buf = String::with_capacity(Self::size_hint());
         self.render_into(&mut buf)?;
         Ok(buf)
     }
-    /// Renders the template to the given `writer` buffer
     fn render_into(&self, writer: &mut fmt::Write) -> fmt::Result {
         write!(writer, "{}", self)
     }
 
-    /// Helper function to inspect the template's mime
     fn mime() -> &'static str
     where
         Self: Sized;
 
     fn size_hint() -> usize;
 }
-
-pub use yarte_config::{read_config_file, Config};
-pub use yarte_derive::*;
-pub use yarte_helpers::*;
 
 #[cfg(feature = "with-actix-web")]
 pub mod actix_web {
@@ -41,7 +36,7 @@ pub fn rerun_if_templates_changed() {
     loop {
         if let Some(dir) = stack.pop() {
             for entry in fs::read_dir(dir).expect("valid directory") {
-                let entry = entry.expect("valid directory");
+                let entry = entry.expect("valid directory entry");
                 let path = entry.path();
                 if path.is_dir() {
                     stack.push(path);
