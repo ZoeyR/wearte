2b1243edc023dc4a1496a1e03337c3cc4565b73b
Juan Aguilar Santillana <mhpoin@gmail.com>
2019-02-23 14:47:58 +0100

    Refactor remove unnecessary visit_dir function

--- yarte/src/lib.rs
+++ yarte/src/lib.rs
@@ -1,9 +1,4 @@
-use std::{
-    fmt,
-    fs::{self, DirEntry},
-    io,
-    path::Path,
-};
+use std::{fmt, fs};
 
 pub use yarte_helpers::{helpers, helpers::MarkupDisplay, Error, Result};
 
@@ -39,35 +34,23 @@ pub mod actix_web {
     };
 }
 
-fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry);
-            }
-        }
-    }
-    Ok(())
-}
-
-/// Build script helper to rebuild crates if contained templates have changed
-///
-/// Iterates over all files in the template directories and writes a
-/// `cargo:rerun-if-changed=` line for each of them to stdout.
-///
-/// This helper method can be used in build scripts (`build.rs`) in crates
-/// that have templates, to make sure the crate gets rebuilt when template
-/// source code changes.
+// TODO:
 pub fn rerun_if_templates_changed() {
     let file = read_config_file();
-    for template_dir in &Config::new(&file).dirs {
-        visit_dirs(template_dir, &|e: &DirEntry| {
-            println!("cargo:rerun-if-changed={}", e.path().to_str().unwrap());
-        })
-        .unwrap();
+    let mut stack = Config::new(&file).dirs;
+    loop {
+        if let Some(dir) = stack.pop() {
+            for entry in fs::read_dir(dir).expect("valid directory") {
+                let entry = entry.expect("valid directory");
+                let path = entry.path();
+                if path.is_dir() {
+                    stack.push(path);
+                } else {
+                    println!("cargo:rerun-if-changed={}", entry.path().to_str().unwrap());
+                }
+            }
+        } else {
+            break;
+        }
     }
 }
